ed7de3cc283ed1a1d3cf09afd78c304b24c1d498
Rokan3583 <joskiytelik@gmail.com>
2024-11-15 16:23:08 +0300

    new work

---
+++ t_b.py
@@ -0,0 +1,16 @@
+import telebot
+
+# Вставьте сюда ваш токен от BotFather
+API_TOKEN = '8039197845:AAGzJmz0FquuqwcF-OIYvPy_4cDFxG8wRPw'
+
+# Создаем объект бота
+bot = telebot.TeleBot(API_TOKEN)
+
+# Обработчик всех сообщений
+@bot.message_handler(func=lambda message: True)
+def handle_all_messages(message):
+    bot.reply_to(message, 'Привет! И тебе всего хорошего!!!!')
+
+if __name__ == '__main__':
+    # Запускаем бесконечный цикл обработки событий
+    bot.polling()
